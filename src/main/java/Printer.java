eec208f59b33a094639cb7633e2cf19827f67320
Pedram Valiani <p.valiani@gmail.com>
2021-01-25 17:23:16 +0000

    can get pages tests working

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,13 @@
 public class Printer {
+
+    private int pages;
+
+    public Printer(int pages){
+        this.pages = pages;
+    }
+
+    public int getPages(){
+        return this.pages;
+    }
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void setUp(){
+
+        printer = new Printer(25);
+    }
+
+    @Test
+    public void getPages(){
+        assertEquals(25, printer.getPages());
+    }
+
 }
