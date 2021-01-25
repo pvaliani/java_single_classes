ed2c999bdc949c52ddbc49621f6ede07f1a50407
Pedram Valiani <p.valiani@gmail.com>
2021-01-25 16:51:35 +0000

    add test passes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,22 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    Calculator calculator;
+
+//    set up a new calculator instance to be used in the test
+    @Before
+    public void setUp(){
+        calculator = new Calculator();
+
+    }
+// test addition
+
     @Test
     public void calculatorCanAdd(){
-
-        Calculator add = new Calculator(1);
-        assertEquals("Earth", planet.getName());
+        assertEquals(4, calculator.add(2,2));
     }
 }
