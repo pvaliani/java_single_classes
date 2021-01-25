f88e5f1c879bedc3ab95c6a95dce61f09b2d896d
Pedram Valiani <p.valiani@gmail.com>
2021-01-25 17:28:29 +0000

    extensions complete

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,13 +1,26 @@
 public class Printer {
 
     private int pages;
+    private int toner;
 
-    public Printer(int pages){
+    public Printer(int pages, int toner){
         this.pages = pages;
+        this.toner = toner;
     }
 
     public int getPages(){
         return this.pages;
+    }
+
+    public void print(int numberOfPages, int copies){
+        if((numberOfPages * copies) <= this.pages){
+            this.pages = this.pages - (numberOfPages * copies);
+            this.toner = this.toner - (numberOfPages * copies);
+        }
+    }
+
+    public int getToner(){
+        return this.toner;
     }
 
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -10,12 +10,42 @@ public class PrinterTest {
     @Before
     public void setUp(){
 
-        printer = new Printer(25);
+        printer = new Printer(25,30);
     }
 
     @Test
     public void getPages(){
         assertEquals(25, printer.getPages());
+    }
+
+    @Test
+    public void printEnoughPages(){
+        printer.print(5, 2);
+        assertEquals(15, printer.getPages());
+
+    }
+
+    @Test
+    public void printNotEnoughPages(){
+        printer.print(5, 6);
+        assertEquals(25, printer.getPages());
+    }
+
+    @Test
+    public void getToner(){
+        assertEquals(30, printer.getToner());
+    }
+
+    @Test
+    public void tonerReducesWhenPrinting(){
+        printer.print(5, 2);
+        assertEquals(20, printer.getToner());
+    }
+
+    @Test
+    public void tonerStaysSameIfNoPrinting(){
+        printer.print(5,6);
+        assertEquals(30, printer.getToner());
     }
 
 }
