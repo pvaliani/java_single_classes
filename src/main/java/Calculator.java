290c787aecaea5acafe6f8710e2f303689441554
Pedram Valiani <p.valiani@gmail.com>
2021-01-25 16:48:07 +0000

    addition method added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,6 @@
 public class Calculator {
+
+   public int add(int num1, int num2){
+       return num1 + num2;
+   }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    public void calculatorCanAdd(){
+
+        Calculator add = new Calculator(1);
+        assertEquals("Earth", planet.getName());
+    }
 }
